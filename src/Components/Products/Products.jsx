de7d5a32f832071d25f2deb359b268e5fa887dc3
maruf-ahmed <marufahamed1110@gmail.com>
2024-08-02 00:04:23 +0600

    Added product card

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -2,6 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import Spinner from "../../Utils/Spinner";
+import { Rate } from "antd";
+import { FiShoppingCart } from "react-icons/fi";
+import { FaRegHeart } from "react-icons/fa";
+import { Slide } from "react-awesome-reveal";
 
 function Products() {
   const categoryName = useParams();
@@ -16,14 +20,61 @@ function Products() {
   });
   console.log(productData);
   return (
-    <div className="mt-20">
+    <div className="mt-28 max-w-7xl mx-auto">
       {isLoading ? (
         <Spinner />
       ) : (
-        <div className="">
+        <div className="grid grid-cols-5 gap-4">
           {productData?.map((data, index) => (
-            <div key={index} className="">
-              <h2>{data?.productName}</h2>
+            <div key={index} className="overflow-hidden relative group">
+              <a
+                href="#"
+                className="group relative block overflow-hidden  border border-gray-200"
+              >
+                <img
+                  src="https://i.ibb.co/tQP5f0k/red-grapes.webp"
+                  alt=""
+                  className=" w-full object-cover transition duration-500 group-hover:scale-105 h-52 p-4 "
+                />
+
+                <div className="relative bg-white p-6 space-y-1">
+                  <div className=" flex items-center gap-4">
+                    <p className="mt-1.5 text-sm text-gray-500 line-through">
+                      ৳ {data?.price?.oldPrice}
+                    </p>
+                    <p className="mt-1.5 text-sm text-gray-900">
+                      ৳ {data?.price?.latestPrice}
+                    </p>
+                    <p className="mt-1.5 text-sm text-red-500">
+                      {data?.price?.off} %
+                    </p>
+                  </div>
+
+                  <h3 className=" text-lg font-medium text-gray-900">
+                    {data?.productName}
+                  </h3>
+                  <div className="flex items-center gap-2">
+                    <Rate defaultValue={data?.rating} />{" "}
+                    <p className="text-sm">({data?.rating})</p>
+                  </div>
+                </div>
+              </a>
+              {/* Overlay section */}
+              <div className="left-0 absolute  text-white top-[100%] duration-500 opacity-0 group-hover:opacity-100 group-hover:top-0  w-full h-full bg-[#36a853]/50 group-hover:backdrop-blur-md">
+                <div className="flex items-center justify-center gap-2 absolute top-[40%] left-1/3">
+                  <Slide direction="down" duration={1000}>
+                    <button className="bg-white rounded-full text-gray-900 h-10 w-10 flex items-center justify-center">
+                      <FiShoppingCart className="" />
+                    </button>
+                    <button className="bg-white rounded-full text-gray-900 h-10 w-10 flex items-center justify-center">
+                      <FaRegHeart className="" />
+                    </button>
+                  </Slide>
+                </div>
+                <button className=" bg-[#36a853] absolute  w-full text-white text-sm py-2 cursor-pointer text-center bottom-0">
+                  Details
+                </button>
+              </div>
             </div>
           ))}
         </div>
