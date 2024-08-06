873484a7c030020e0720b6dc3fa87a4f41748473
maruf-ahmed <marufahamed1110@gmail.com>
2024-08-06 19:10:50 +0600

    Added new details in product details page

--- src/Components/Products/ProductDetails.jsx
+++ src/Components/Products/ProductDetails.jsx
@@ -5,7 +5,12 @@ import { FaRegHeart } from "react-icons/fa";
 import { FiShare2 } from "react-icons/fi";
 import { useParams } from "react-router-dom";
 import { BsBookmarks } from "react-icons/bs";
-
+import { BiErrorAlt } from "react-icons/bi";
+import { FiShoppingCart } from "react-icons/fi";
+import { IoLocationOutline } from "react-icons/io5";
+import { BsCashCoin } from "react-icons/bs";
+import { MdOutlineKeyboardReturn } from "react-icons/md";
+import { RiAwardLine } from "react-icons/ri";
 function ProductDetails() {
   const { id } = useParams();
   console.log(id);
@@ -22,7 +27,7 @@ function ProductDetails() {
 
   return (
     <div className="mt-28 max-w-7xl mx-auto">
-      <div className="grid grid-cols-7 gap-8">
+      <div className="grid grid-cols-8 gap-6">
         <div
           id="megnifyImg_Area"
           className="border border-gray-300 overflow-hidden relative col-span-2  flex items-center justify-center "
@@ -56,6 +61,9 @@ function ProductDetails() {
                 {" "}
                 ৳ {productDetailsData?.price.oldPrice}
               </h1>
+              <p className="bg-red-600 text-white px-2 py-1 rounded-full rounded-r-none font-semibold">
+                {productDetailsData?.price?.off} % Off
+              </p>
             </div>
             <div className="pt-5 space-y-1">
               <p className="text-orange-500 text-sm flex items-center gap-2">
@@ -68,11 +76,11 @@ function ProductDetails() {
                 ফ্রি! এছাড়াও ২৯% পর্যন্ত ছাড়!
               </p>
             </div>
-            <div className="pt-5 text-white">
-              <div className="bg-orange-500 items-center rounded-xl p-4 grid grid-cols-4 gap-4">
+            <div className="pt-5 text-white hidden">
+              <div className="bg-orange-500  rounded-xl p-4 grid grid-cols-4 gap-2">
                 <div className="col-span-1">
                   <h2 className="text-2xl font-semibold">
-                    Sale : ৳ {productDetailsData?.price?.latestPrice}
+                    Sale ৳ {productDetailsData?.price?.latestPrice}
                   </h2>
                   <p>Start in 1 days</p>
                 </div>
@@ -81,10 +89,15 @@ function ProductDetails() {
                   <p className=" text-sm">
                     আমাদের অনলাইন শপিং সেলে আপনাকে স্বাগতম! এখানে আপনার পছন্দের
                     পণ্যগুলো সবচেয়ে সাশ্রয়ী মূল্যে পেতে পারেন। আমাদের বিশেষ অফার
-                    এবং ডিসকাউন্টের মাধ্যমে আপনার কেনাকাটা করুন সহজেই, ঘরে বসে।
                   </p>
                 </div>
               </div>
+            </div>
+            <div className="mt-3">
+              <button className="btn text-white bg-[#36a853]">
+                <FiShoppingCart className="text-xl" />
+                Add to Cart
+              </button>
             </div>
           </div>
           <div className=" flex  gap-3">
@@ -92,7 +105,41 @@ function ProductDetails() {
             <FaRegHeart className="text-2xl text-gray-500" />
           </div>
         </div>
-        <div className=" col-span-1">world</div>
+        <div className=" col-span-2 bg-gray-50 p-4">
+          <div className="flex items-center justify-between text-sm font-medium text-gray-500">
+            <p>Devlivery Options</p>
+            <BiErrorAlt />
+          </div>
+
+          <div className="mt-4 space-y-3">
+            <div className="flex items-center gap-3">
+              <IoLocationOutline className="text-2xl" />
+              <p>Barishal, Barishal - Agailjhara, Gaila</p>
+            </div>
+            <div className="flex items-center gap-3">
+              <BsCashCoin className="text-xl" />
+              <p>Cash on Delivery Available</p>
+            </div>
+          </div>
+          <div className="py-6">
+            <hr />
+          </div>
+          <div className="flex items-center justify-between text-sm font-medium text-gray-500">
+            <p>Return & Warranty </p>
+            <BiErrorAlt />
+          </div>
+
+          <div className="mt-4 space-y-3">
+            <div className="flex items-center gap-3">
+              <MdOutlineKeyboardReturn className="text-xl" />
+              <p>7 Days Return</p>
+            </div>
+            <div className="flex items-center gap-3">
+              <RiAwardLine className="text-xl" />
+              <p>Warranty not available</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
