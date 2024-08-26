ce0714d3bc0f24b28f9d870393769f01b052c9f8
maruf-ahmed <marufahamed1110@gmail.com>
2024-08-27 01:23:05 +0600

    update fail payment and cancel payment

--- src/Pages/Payment/CanclePayment.jsx
+++ src/Pages/Payment/CanclePayment.jsx
@@ -1,9 +1,23 @@
 
-
+import { TiCancel } from "react-icons/ti";
 function CanclePayment() {
   return (
-    <div className="mt-28">
-      Cancle payment
+    <div className="mt-28 flex items-center justify-center h-[400px]">
+      <div className="flex flex-col items-center space-y-4">
+        <div className=" h-28 w-28 rounded-full bg-orange-200 flex items-center justify-center">
+          <div className="bg-orange-500 h-16 w-16 rounded-full flex items-center justify-center">
+            <TiCancel  className="text-white text-2xl" />
+          </div>
+        </div>
+
+        <div className=" text-center">
+          {/* <h2 className="text-2xl font-semibold text-gray-800">Thank You</h2> */}
+
+          <h2 className="text-2xl font-semibold text-gray-800">
+          Your payment process has been cancelled
+          </h2>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Pages/Payment/FailPayment.jsx
+++ src/Pages/Payment/FailPayment.jsx
@@ -1,10 +1,24 @@
 
-
+import { RxCross2 } from "react-icons/rx";
 function FailPayment() {
   return (
-    <div className="mt-28">
-      Fail Payment
+    <div className="mt-28 flex items-center justify-center h-[400px]">
+    <div className="flex flex-col items-center space-y-4">
+      <div className=" h-28 w-28 rounded-full bg-red-200 flex items-center justify-center">
+        <div className="bg-red-500 h-16 w-16 rounded-full flex items-center justify-center">
+          <RxCross2   className="text-white text-xl" />
+        </div>
+      </div>
+
+      <div className=" text-center">
+       
+
+        <h2 className="text-2xl font-semibold text-gray-800">
+        Your payment process has been Failed
+        </h2>
+      </div>
     </div>
+  </div>
   )
 }
 
