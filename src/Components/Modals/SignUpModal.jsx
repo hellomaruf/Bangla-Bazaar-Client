7ec10808dce72c4945f7e58498ea1fa5694abd76
maruf-ahmed <marufahamed1110@gmail.com>
2024-08-29 12:25:20 +0600

    save user in database

--- src/Components/Modals/SignUpModal.jsx
+++ src/Components/Modals/SignUpModal.jsx
@@ -5,6 +5,7 @@ import upload from "../../assets/Imgs/upload.png";
 import { AuthContaxt } from "../../Services/AuthProvider";
 import { ImageUpload } from "../../Utils/imgUpload";
 import toast from "react-hot-toast";
+import axios from "axios";
 
 function SignUpModal({ isOpenSignUp, signUpClose }) {
   const [avatarURL, setAvatarURL] = useState(upload);
@@ -33,6 +34,13 @@ function SignUpModal({ isOpenSignUp, signUpClose }) {
     const password = form.password.value;
     const photo = await ImageUpload(image);
     console.log(name, email, password, photo);
+    const userInfo = {
+      name,
+      email,
+      password,
+      photo,
+      role: "user",
+    };
     createNewUser(email, password)
       .then((res) => {
         updateUserProfile(name, photo);
@@ -40,6 +48,15 @@ function SignUpModal({ isOpenSignUp, signUpClose }) {
           toast.success("Successfully SignUp!");
           console.log(res.user);
         }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    await axios
+      .post(`${import.meta.env.VITE_LOCALHOST_URL}/user`, userInfo)
+      .then((res) => {
+        console.log(res.data);
       })
       .catch((error) => {
         console.log(error);
@@ -132,37 +149,6 @@ function SignUpModal({ isOpenSignUp, signUpClose }) {
                   </div>
                 </form>
 
-                <div className="flex items-center justify-between mt-4">
-                  <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/5"></span>
-
-                  <a
-                    href="#"
-                    className="text-xs text-center text-gray-500 uppercase hover:underline"
-                  >
-                    or login with Social Media
-                  </a>
-
-                  <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/5"></span>
-                </div>
-
-                <div className="flex items-center mt-6 -mx-2">
-                  <button
-                    type="button"
-                    className="flex items-center justify-center w-full px-6 py-2 mx-2 text-sm font-medium text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:bg-blue-400 focus:outline-none"
-                  >
-                    <svg
-                      className="w-4 h-4 mx-2 fill-current"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z"></path>
-                    </svg>
-
-                    <span className="hidden mx-2 sm:inline">
-                      Sign in with Google
-                    </span>
-                  </button>
-                </div>
-
                 <p className="mt-8 text-xs font-light text-center text-gray-400">
                   {" "}
                   Dont have an account?{" "}
@@ -170,7 +156,7 @@ function SignUpModal({ isOpenSignUp, signUpClose }) {
                     href="#"
                     className="font-medium text-[#36A853]  hover:underline"
                   >
-                    Create One
+                    Sign In
                   </a>
                 </p>
               </div>
