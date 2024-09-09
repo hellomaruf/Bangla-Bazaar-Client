cc7113d6dc227e8ddf382b07158ca044bfecbed5
maruf-ahmed <marufahamed1110@gmail.com>
2024-09-09 10:41:37 +0600

    Added some problem solving

--- src/Components/Home/Stats.jsx
+++ src/Components/Home/Stats.jsx
@@ -8,28 +8,28 @@ function Stats() {
       <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-4 items-center justify-center gap-4 ">
         <div className="flex rounded-xl  p-6 gap-5  border-2 border-gray-300  items-center justify-center">
           <img className="w-10" src={img1} alt="" />
-          <p className="text-xl">
+          <p className="text-lg md:text-xl">
             <span className="font-bold text-[#3aa753]">+15000 products</span> to
             shop from
           </p>
         </div>
         <div className="flex rounded-xl p-6 gap-5  border-2 border-gray-300 items-center justify-center">
           <img className="w-10" src={img2} alt="" />
-          <p className="text-xl">
+          <p className="text-lg md:text-xl">
             <span className="font-bold text-[#3aa753]">Pay after</span>{" "}
             receiving products
           </p>
         </div>
         <div className="flex rounded-xl p-6 gap-5  border-2 border-gray-300 items-center justify-center">
           <img className="w-10" src={img3} alt="" />
-          <p className="text-xl">
+          <p className="text-lg md:text-xl">
             Get your delivery within{" "}
             <span className="font-bold text-[#3aa753]">1 hour</span>
           </p>
         </div>
         <div className="flex rounded-xl p-6 gap-5  border-2 border-gray-300 items-center justify-center">
           <img className="w-10" src={img4} alt="" />
-          <p className="text-xl">
+          <p className="text-lg md:text-xl">
             <span className="font-bold text-[#3aa753]">Get offers</span> that
             Save Money
           </p>
